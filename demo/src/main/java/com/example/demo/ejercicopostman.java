5c57414118575ac110a4a66b7ad5ada6986b3185
alvaroF01 <alvaro.fernandez@bosonit.com>
2022-01-20 14:23:06 +0100

    Get

---
+++ demo/src/main/java/com/example/demo/ejercicopostman.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController()
+@RequestMapping("")
+public class ejercicopostman {
+    @GetMapping("/user/{name}")
+    public String getName(@PathVariable(name="name") String nombre){
+        String nameToreturn = nombre;
+        return "hola " + nameToreturn ;
+    }
+}
+
+
+
